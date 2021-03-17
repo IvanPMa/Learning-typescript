b4652a11e2e456c58d6f7cc04e77a50206c776a2
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-17 14:13:42 -0600

    Funciones

---
+++ Section-3/function.js
@@ -0,0 +1,6 @@
+function sumar(a, b) {
+    return a + b;
+}
+var suma = sumar(3, 1);
+console.log(suma);
+console.log(sumar(2, 2));
---
+++ Section-3/function.ts
@@ -0,0 +1,8 @@
+function sumar (a:number, b:number):number{
+    return a + b
+}
+
+let suma:number = sumar (3,1)
+console.log(suma);
+
+console.log(sumar(2,2))
