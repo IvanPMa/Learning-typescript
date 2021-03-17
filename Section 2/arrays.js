b545173b03eccadf47521cea8e046df9affeeafa
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-17 13:24:46 -0600

    Arrays

---
+++ Section 2/arrays.js
@@ -0,0 +1,5 @@
+var numeros = [1, 2, 3, 4];
+var nombres = ['Ivan', 'Pacheco'];
+var collecionDeNumeros = [1, 2, 3, 4];
+var collecionDeNombres = ['Ivan', 'Pacheco'];
+var siono = [true, false];
---
+++ Section 2/arrays.ts
@@ -0,0 +1,8 @@
+let numeros:number[] = [1,2,3,4]
+let nombres: string[] = ['Ivan', 'Pacheco']
+
+let collecionDeNumeros: Array<number> = [1,2,3,4]
+let collecionDeNombres: Array<string> = ['Ivan', 'Pacheco']
+
+
+let siono: boolean[] = [true,false]
