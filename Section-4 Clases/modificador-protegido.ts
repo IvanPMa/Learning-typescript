1a3181ef51cf2272ced9f69186d4705513573cee
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-20 19:13:27 -0600

    Modificador protegido

---
+++ Section-4 Clases/modificador-protegido.ts
@@ -0,0 +1,21 @@
+class Padre{
+    protected nombre:string
+    public constructor(nombre_:string){
+        this.nombre = nombre_
+    }
+}
+
+class Hijo extends Padre{
+    public apellido: string
+
+    constructor (nombre_:string, apellido_:string){
+        super(nombre_)
+        this.apellido = apellido_
+    }
+
+    mostrarNombre(): void {
+        console.log(this.nombre)
+    }
+}
+const objeto = new Hijo('Ivan', 'Pacheco')
+// objeto.
