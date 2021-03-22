0bd5ed884829aae8e7e7ee92d13f7a7801330570
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-22 12:58:03 -0600

    Any y el error

---
+++ Section-6 Tipado Generico/el-error.ts
@@ -0,0 +1,9 @@
+const noombre:string = 'Ivan'
+const numero:number = 22
+let nolose:any = 'Manuel'
+// nolose.
+
+//El error que genera el utilizar el any que no puedo usar una funcion o metodo
+// dependiendo del tipo de dato genrico, cuando genero tipos de dato any siempre
+// sera any y no podre trbajar sobre el a difenrencia que del tipado generico 
+// que trasforma al tipo de dato que retorna
