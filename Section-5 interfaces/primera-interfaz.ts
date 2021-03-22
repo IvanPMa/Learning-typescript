00a3475697bcd648362eb4c66fd0dba8bf6d6847
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-21 21:10:42 -0600

    Interfaz

--- Section-5 interfaces/primera-interfaz.ts
+++ Section-5 interfaces/primera-interfaz.ts
@@ -1,14 +1,13 @@
 //Nos permite crear variables del tipo de la interfaz y acceder a sus atributos
-interface Persona {
+interface IPersona {
     // Atributos 
     nombre :string
     apellido: string
 }
 
-function caminar(persona:Persona):void{
+function caminar(persona:IPersona):void{
     console.log('La persona ' + persona.nombre,persona.apellido+' esta caminando');
 }
-let nueva_persona:Persona = { nombre: 'ivan',apellido:'Pacheco'}
+let nueva_persona:IPersona = { nombre: 'ivan',apellido:'Pacheco'}
 console.log (nueva_persona);
-
 caminar (nueva_persona)
